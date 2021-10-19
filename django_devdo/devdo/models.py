ba01cfcd2978db7216631532b167ff411e17665d
Allison Moss <allisonwmoss@icloud.com>
2021-10-19 13:22:45 -0400

    Listen I do not remember what I changed but it has all been important

--- django_devdo/devdo/models.py
+++ django_devdo/devdo/models.py
@@ -8,7 +8,11 @@ class Idea(models.Model):
     title = models.CharField(max_length=240)
     posted = models.DateField(auto_now_add=True)
     tags = TaggableManager()
-    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_story = models.CharField(max_length=250)
+    poster = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="poster")
+    dev = models.ForeignKey(User, null=True, blank=True,
+                            on_delete=models.CASCADE, related_name="dev")
     in_progress = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
 
--- django_devdo/devdo/serializers.py
+++ django_devdo/devdo/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
 from taggit.serializers import (TagListSerializerField, TaggitSerializer)
 from .models import Idea
+from django.contrib.auth.models import User
 
 
 class IdeaSerializer(serializers.ModelSerializer, TaggitSerializer):
+    tags = TagListSerializerField()
+    poster = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='username'
+    )
+    dev = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='username'
+    )
 
     class Meta:
-        tags = TagListSerializerField()
         model = Idea
-        fields = ('id', 'title', 'posted',
-                  'poster', 'in_progress', 'complete')
+        fields = ('id', 'title', 'posted', 'user_story',
+                  'poster', 'dev', 'in_progress', 'complete', 'tags')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
